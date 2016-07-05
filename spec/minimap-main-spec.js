d782ed650775d82d32e3cd0c18105ca7e82acb59
abe33 <cedric.nehemie@gmail.com>
2016-07-05 15:57:11 +0200

    :bug: Add guard against duplicated minimap
    Fixes #504

--- spec/minimap-main-spec.js
+++ spec/minimap-main-spec.js
@@ -63,6 +63,19 @@ describe('Minimap package', () => {
       it('removes the minimap from their editor parent', () => {
         expect(editorElement.shadowRoot.querySelector('atom-text-editor-minimap')).not.toExist()
       })
+
+      fdescribe('and reactivated with a remaining minimap in the DOM', () => {
+        beforeEach(() => {
+          const m = new Minimap({textEditor: editor})
+          const v = atom.views.getView(m)
+          editorElement.shadowRoot.appendChild(v)
+          waitsForPromise(() => atom.packages.activatePackage('minimap'))
+        })
+
+        it('removes the remaining minimap', () => {
+          expect(editorElement.shadowRoot.querySelectorAll('atom-text-editor-minimap').length).toEqual(1)
+        })
+      })
     })
   })
 
