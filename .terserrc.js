b3d00af688fe31f4e229471fe4cb2853291b8024
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-29 06:59:46 -0600

    fix: add higher optimizations to Terser
    By applying more optimizations the code size is reduced.
    
    Reuces the size from
    145.44 KB  + 61 KB
    to
    134.76 KB  +   57.42 KB

---
+++ .terserrc.js
@@ -0,0 +1,30 @@
+const isDev = process.env.NODE_ENV !== "production"
+
+module.exports = {
+  // "module": false, // controlled by Parcel
+  "compress": {
+    "ecma": "2018", // Change based on the target
+    // "toplevel": true, // controlled by Parcel
+    "hoist_vars": true,
+    "hoist_funs": true,
+    "pure_getters": true,
+    "unsafe": true,
+    "unsafe_arrows": true,
+    "unsafe_comps": true,
+    "unsafe_Function": true,
+    "unsafe_math": true,
+    "unsafe_symbols": true,
+    "unsafe_methods": true,
+    "unsafe_proto": true,
+    "unsafe_regexp": true,
+    "unsafe_undefined": true,
+    "passes": isDev ? 0 : 2,
+  },
+  "parse": {
+    "ecma": 2020
+  },
+  "mangle": !isDev,
+  "format": {
+    "beautify": isDev
+  },
+}
