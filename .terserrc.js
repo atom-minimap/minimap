c10e638c43f28d6287e6636c415c3389c3ba3e9c
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-02 00:19:59 -0500

    chore: disable terser compression when dev

--- .terserrc.js
+++ .terserrc.js
@@ -1,31 +1,33 @@
-const isDev = process.env.NODE_ENV !== "production"
+const isDev = process.env.NODE_ENV === "development"
 const isTest = process.env.NODE_ENV === "test"
 const isReadable = process.env.READABLE_BUILD || isDev || isTest
 
-export default {
+const TerserOptions = {
   // "module": false, // controlled by Parcel
-  compress: {
-    global_defs: {
-      // remove spec specific code for production
-      "@atom.inSpecMode": !isTest ? "() => false" : "() => true",
-    },
-    ecma: "2018", // Change based on the target
-    toplevel: false,
-    hoist_vars: false,
-    hoist_funs: true,
-    pure_getters: true,
-    unsafe: true,
-    unsafe_arrows: true,
-    unsafe_comps: true,
-    unsafe_Function: true,
-    unsafe_math: true,
-    unsafe_symbols: true,
-    unsafe_methods: true,
-    unsafe_proto: true,
-    unsafe_regexp: true,
-    unsafe_undefined: true,
-    passes: isDev ? 0 : 2,
-  },
+  compress: isDev
+    ? false
+    : {
+        global_defs: {
+          // remove spec specific code for production
+          "@atom.inSpecMode": !isTest ? "() => false" : "() => true",
+        },
+        ecma: "2018", // Change based on the target
+        toplevel: false,
+        hoist_vars: false,
+        hoist_funs: true,
+        pure_getters: true,
+        unsafe: true,
+        unsafe_arrows: true,
+        unsafe_comps: true,
+        unsafe_Function: true,
+        unsafe_math: true,
+        unsafe_symbols: true,
+        unsafe_methods: true,
+        unsafe_proto: true,
+        unsafe_regexp: true,
+        unsafe_undefined: true,
+        passes: 2,
+      },
   parse: {
     ecma: 2020,
   },
@@ -34,3 +36,4 @@ export default {
     beautify: isReadable,
   },
 }
+export default TerserOptions
